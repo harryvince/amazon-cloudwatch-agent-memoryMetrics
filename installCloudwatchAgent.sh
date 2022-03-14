169cab3e5029230704bd91bbdd08562b6401f714
Harry Vince <47283812+harryvince@users.noreply.github.com>
2022-03-14 13:28:29 +0000

    Updating to log bash history to ec2

--- installCloudwatchAgent.sh
+++ installCloudwatchAgent.sh
@@ -2,6 +2,12 @@
 cd /tmp
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 rpm -U ./amazon-cloudwatch-agent.rpm
+context="export PROMPT_COMMAND=""'"'RETRN_VAL=$?;logger -p local6.debug "$(whoami) [$$]: $(history 1 | sed "s/[ ][0–9]+[ ]//" ) [$RETRN_VAL]"'"'"
+echo $context > /etc/profile
+source /etc/profile
+echo 'local6.* /var/log/commands.log' > /etc/rsyslog.d/bash.conf
+echo '/var/log/commands.log' > /etc/logrotate.d/rsyslog
+service rsyslog restart
 echo '{
    "metrics":{
       "namespace": "LinuxServer",
@@ -16,7 +22,22 @@ echo '{
       "append_dimensions":{
          "InstanceId": "${aws:InstanceId}"
       }
-   }
+   },
+   "logs": {
+      "logs_collected": {
+        "files": {
+          "collect_list": [
+            {
+                "file_path": "/var/log/commands.log",
+                "log_group_name":  "/ec2/bash-history/",
+                "log_stream_name": "{instance_id}",
+                "timestamp_format": "%d/%b/%Y:%H:%M:%S %z",
+                "timezone": "Local"
+            }
+          ]
+        }
+      }
+    }
 } ' >  /opt/aws/amazon-cloudwatch-agent/bin/config.json
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
 systemctl enable amazon-cloudwatch-agent
